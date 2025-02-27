d0951898deaa3176eaf0600ab64ee9dcc6c9a209
Valbert Vieira <programador02sundownpark@gmail.com>
2025-02-27 14:25:07 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
 
-const greetMsg = ref("");
-const name = ref("");
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
 
 import { check } from '@tauri-apps/plugin-updater';
 import { relaunch } from '@tauri-apps/plugin-process';
@@ -39,36 +31,26 @@ onMounted(async () => {
       }
     });
   
-    console.log('update installed');
-    await relaunch();
+    
   }
 
 })
+
+async function restartApp(){
+  await relaunch();
+}
+
 
 </script>
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue V2</h1>
+    <h1>Tauri In App update Demo + Vue V5</h1>
     {{ downloaded, contentLength }}
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
+    
+    <button @click="restartApp">
+        Restartar app
+    </button>
   </main>
 </template>
 
